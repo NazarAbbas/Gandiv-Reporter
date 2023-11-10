0dfafa675d79b70af6a31143a53292437ad7ad99
Nazar Abbas <nnazarabbas07@gmail.com>
2023-11-10 09:06:45 +0530

    commit untracked files

--- lib/constants/values/app_colors.dart
+++ lib/constants/values/app_colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color colorPrimary =
-      const Color(0xFF3e3e42); // const Color(0xFF007BFF);
+      const Color(0xFF3e3e42); // //const Color(0xFF007BFF);
   static Color lightGray = const Color(0xFFF8F8F8);
   static Color white = const Color(0xFFFFFFFF);
   static Color black = const Color(0xFF000000);
